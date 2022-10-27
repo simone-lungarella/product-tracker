cb8908c2c5bef3a9fff37bec54e04be3199ea2a4
Simone Lungarella <simonelungarella@gmail.com>
2022-10-27 19:40:15 +0200

    feat: setup tailwind and basic routing

---
+++ src/Hompage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Homepage() {
+    console.log('Homepage');
+    return (
+        <div>
+            <h1 className='text-3xl font-bold underline' >Homepage</h1>
+        </div>
+    );
+}
+
+export default Homepage;
